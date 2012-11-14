053cdd6b2073d04b02c194986adff1c56dcd83fe
Michiel <info@fastfox.nl>
2012-11-14 14:41:47 +0100

    Feedback colors toggle

--- javascript.js
+++ javascript.js
@@ -6,11 +6,30 @@ $(document).ready(function() {
 
   $('.feedback-good').bind( 'click', function(e) {
     console.log( 'Good' );
+    if( !$(this).hasClass('ui-icon-plus-green') ) {
+      $(this).removeClass('ui-icon-plus').addClass('ui-icon-plus-green');
+      var bad = $(this).siblings('.feedback-bad');
+      if( bad.hasClass('ui-icon-minus-red') ) {
+        bad.removeClass('ui-icon-minus-red').addClass('ui-icon-minus');
+      } 
+    } else {  
+      $(this).removeClass('ui-icon-plus-green').addClass('ui-icon-plus');
+    }
     return false;
   } );
 
   $('.feedback-bad').bind( 'click', function(e) {
     console.log( 'Bad' );
+    if( !$(this).hasClass('ui-icon-minus-red') ) {
+      $(this).removeClass('ui-icon-minus').addClass('ui-icon-minus-red');
+      var good = $(this).siblings('.feedback-good');
+      if( good.hasClass('ui-icon-plus-green') ) {
+        good.removeClass('ui-icon-plus-green').addClass('ui-icon-plus');
+      }
+    } else {  
+      $(this).removeClass('ui-icon-minus-red').addClass('ui-icon-minus');
+    }
+
     return false;
   } );
 
