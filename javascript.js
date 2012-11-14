1004b02d879bd4dfc854b3daf7a99a709d8ad24f
Michiel Vos <info@fastfox.nl>
2012-11-14 10:29:21 +0100

    Playlist feedback buttons

--- javascript.js
+++ javascript.js
@@ -1,3 +1,17 @@
 $('#searchSubmit').bind( 'click', function( e ) {
   alert( 'hoi' );
 } );
+
+$(document).ready(function() {
+
+  $('.feedback-good').bind( 'click', function(e) {
+    console.log( 'Good' );
+    return false;
+  } );
+
+  $('.feedback-bad').bind( 'click', function(e) {
+    console.log( 'Bad' );
+    return false;
+  } );
+
+} );
