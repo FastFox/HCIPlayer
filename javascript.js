e055c10c2d9a080b2597833ad398cbee28f4d298
Michiel <info@fastfox.nl>
2012-11-14 18:01:38 +0100

    Add track test

--- javascript.js
+++ javascript.js
@@ -33,4 +33,23 @@ $(document).ready(function() {
     return false;
   } );
 
+$('#playlistItems').append( '<li data-role="collapsible" data-mini="true" data-collapsed-icon="arrow-r" data-expanded-icon="arrow-d" data-inset="false" data-content-theme="b">\
+        <h2>\
+          <span style="float: left;">Track C</span>\
+          <span class="feedback-bad ui-icon ui-icon-minus" style="float: right"></span>\
+          <span style="float: right">&nbsp;</span>\
+          <span class="feedback-good ui-icon ui-icon-plus" style="float: right">&nbsp;</span>\
+        </h2>\
+        <ul>\
+          <li>Album: <a href="index.html">Hoi</a></li>\
+		      <li>Cover: <img src="" alt="cover" /></li>\
+		      <li>Tags: <a href="index.html">A</a> <a href="index.html">B</a></li>\
+			    <li>Feedback Score: + 2  - 1</a></li>\
+		    </ul>\
+	    </li>');
+    $('#playlistItems ul li:first').listview('refresh', true);
+  $('#playlistItems').bind('pageinit', function() {
+    $('#playlistItems').listview('refresh');
+  });
+
 } );
