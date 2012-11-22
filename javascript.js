7976a48afcd937f8d72d7ce57448a211562ba362
Michiel <info@fastfox.nl>
2012-11-22 15:28:06 +0100

    Better addTrack

--- javascript.js
+++ javascript.js
@@ -1,12 +1,3 @@
-var data = new function() {
-  this.playlist = new Array();
-  this.searchlist = new Array();
-  
-}
-
-console.log( data );
-
-
 function rebindFeedback( ) {
   $('.feedback-good').unbind('click').bind( 'click', function(e) {
     console.log( 'Good' );
@@ -38,8 +29,68 @@ function rebindFeedback( ) {
   } );
 }
 
+var data = new function( ) {
+  this.playlist = new Array();
+  this.searchlist = new Array();
+  
+  this.pushPlaylist = function( track, isNewTrack ) {
+    isNewTrack = typeof isNewTrack !== 'undefined' ? isNewTrack : true;
+
+    //console.log( $('#playlistItems') );
+    this.playlist.push( track );
+    $('#playlistItems').append( '<li data-role="collapsible" data-mini="true" data-collapsed-icon="arrow-r" data-expanded-icon="arrow-d" data-inset="false" data-content-theme="b">\
+          <h2>\
+            <span style="float: left;">' + track.artist +' &ndash; ' + track.title + '</span>\
+            <span class="feedback-bad ui-icon ui-icon-minus" style="float: right"></span>\
+            <span style="float: right">&nbsp;</span>\
+            <span class="feedback-good ui-icon ui-icon-plus" style="float: right">&nbsp;</span>\
+          </h2>\
+          <ul>\
+            <li>Album: <a href="index.html">Hoi</a></li>\
+		        <li>Cover: <img src="" alt="cover" /></li>\
+		        <li>Tags: <a href="index.html">A</a> <a href="index.html">B</a></li>\
+			      <li>Feedback Score: + 2  - 1</a></li>\
+		      </ul>\
+	      </li>');
+    $( "#playlistItems" ).collapsibleset( "refresh" );
+    rebindFeedback();
+
+    if( isNewTrack ) {
+      var newTrack = $('#playlistItems li:last-child a').addClass('ui-btn-active');
+
+      setTimeout( function() { 
+        newTrack.removeClass('ui-btn-active');
+      }, 3000 );
+    }
+  }
+
+  this.pushSearchlist = function( track ) {
+    this.searchlist.push( track );
+    //console.log( track );
+    $('#resultItems').append( '<li data-role="collapsible" data-mini="true" data-collapsed-icon="arrow-r" data-expanded-icon="arrow-d" data-inset="false" data-content-theme="b">\
+          <h2 class="active">\
+            <span style="float: left;">' + track.artist +' &ndash; ' + track.title + '</span>\
+            <span class="addTrack ui-icon ui-icon-plus" style="float: right">&nbsp;</span>\
+          </h2>\
+          <ul>\
+            <li>Album: <a href="index.html">Hoi</a></li>\
+		        <li>Cover: <img src="" alt="cover" /></li>\
+		        <li>Tags: <a href="index.html">A</a> <a href="index.html">B</a></li>\
+			      <li>Feedback Score: + 2  - 1</a></li>\
+		      </ul>\
+	      </li>');
+      //$( "#resultItems" ).collapsibleset( "refresh" );
+  }
+}
+
+//console.log( data );
+
+
+
+
+
 $(document).ready(function() {
-  rebindFeedback();
+  /* rebindFeedback();
 
 
   $('#playlistItems').append( '<li data-role="collapsible" data-mini="true" data-collapsed-icon="arrow-r" data-expanded-icon="arrow-d" data-inset="false" data-content-theme="b">\
@@ -58,7 +109,7 @@ $(document).ready(function() {
 	    </li>');
 
     $( "#playlistItems" ).collapsibleset( "refresh" );
-    rebindFeedback();
+    rebindFeedback(); */
 
     /*
     console.log( $( '#resultItems' ) );
@@ -105,19 +156,63 @@ function addTrack( track ) {
 			    <li>Feedback Score: + 2  - 1</a></li>\
 		    </ul>\
 	    </li>');
-    $( "#playlistItems" ).collapsibleset( "refresh" );
+    //$( "#playlistItems" ).collapsibleset( "refresh" );
     rebindFeedback();
     //var newTrack = $('#playlistItems .active a').addClass('ui-btn-active').parent().removeClass('active');
     var newTrack = $('#playlistItems .active a').addClass('ui-btn-active');
     newTrack.parent().removeClass('active');
-
     setTimeout( function() { 
          //$('#playlistItems .ui-btn-active').removeClass('ui-btn-active')
          newTrack.removeClass('ui-btn-active');
-         console.log(newTrack);
     }, 3000 );
     //window.location = '#playlist';
 }
+
+$( '#playlist' ).live( 'pageinit', function( event ) {
+  //console.log( '#playlist' );
+  data.pushPlaylist( { 
+    spotify: "123",
+    artist: "Nitrous Oxide",
+    title: "Alderaan"
+  }, false );
+
+  data.pushPlaylist( { 
+    spotify: "123",
+    artist: "Nitrous Oxide",
+    title: "North Pole (Album Edit)"
+  }, false );
+
+  data.pushPlaylist( { 
+    spotify: "123",
+    artist: "Nitrous Oxide",
+    title: "Mirror's Edge"
+  }, false );
+
+  data.pushPlaylist( { 
+    spotify: "123",
+    artist: "Nitrous Oxide",
+    title: "Follow You Ft. Aneym"
+  }, false );
+
+  data.pushPlaylist( { 
+    spotify: "123",
+    artist: "Nitrous Oxide",
+    title: "Muriwai"
+  }, false );
+
+  data.pushSearchlist( {
+    spotify: "123",
+    artist: "Nitrous Oxide",
+    title: "Blurry Motion"
+  } );
+
+  data.pushSearchlist( {
+    spotify: "123",
+    artist: "Nitrous Oxide",
+    title: "Downforce"
+  } );
+
+} );
 
 $( '#addTrack' ).live( 'pagebeforecreate',function(event){
 
@@ -136,15 +231,24 @@ $( '#addTrack' ).live( 'pagebeforecreate',function(event){
 
     $('#resultItems').css('visibility', 'visible');
     $('#resultItems li h2 .addTrack').unbind('click').bind('click', function(e) {
-      addTrack( $(this).parent().children().first().text() );
+      //addTrack( $(this).parent().children().first().text() );
+      //console.log( data.playlist );
+
+      data.pushPlaylist( data.searchlist[$(this).parent().parent().parent().parent().parent().index()] );
+      $(this).removeClass('addTrack').removeClass('ui-icon-plus').addClass('ui-icon-none');
+      return false;
+
     });
 
   });
 
 
   $('#resultItems li h2 .addTrack').bind('click', function(e) {
-    //console.log( 'hoi', $(this).parent().children().first() );
-    addTrack( $(this).parent().children().first().text() );
+    console.log( 'hoi', $(this).parent().children().first() );
+    //addTrack( $(this).parent().children().first().text() );
+    data.pushPlaylist( data.searchlist[$(this).parent().parent().parent().parent().parent().index()] );
+    $(this).removeClass('ui-icon-plus').addClass('ui-icon-none');
+    return false;
   });
 
 
